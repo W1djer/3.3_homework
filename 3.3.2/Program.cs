d39bc5e29c82a870765e1877012736057bbb1dea
Evgenii <w1djer@mail.ru>
2023-03-12 19:56:09 +0300

    Задание 2

---
+++ 3.3.2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("x1 = ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("y1 = ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("z1 = ");
+double z1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("x2 = ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("y2 = ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("z2 = ");
+double z2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine($"Растояние {Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2))}");
