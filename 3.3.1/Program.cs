5d08e9e0aefe4e5156fd041b16579956623cfc6a
Evgenii <w1djer@mail.ru>
2023-03-12 19:53:58 +0300

    Задание 1

---
+++ 3.3.1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите пятизначное число ");
+int n = Convert.ToInt32(Console.ReadLine());
+int x1 = n / 10000;
+int x2 = n / 1000 % 10;
+int y1 = n % 10;
+int y2 = n / 10 % 10;
+if (x1 == y1 && x2 == y2)
+    Console.WriteLine ("Число палиндром");
+else
+    Console.WriteLine("Число не палиндром");
